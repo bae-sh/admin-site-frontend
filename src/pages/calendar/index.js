ab4827e2a1e28069f77f0630308aaa2eb673e46d
bae-sh <hyeon1293@gmail.com>
2022-04-07 01:34:45 +0900

    feat : 일정 추가 삭제 및 refactoring

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -3,189 +3,80 @@ import moment from 'moment';
 import 'moment/locale/ko';
 import { GoChevronLeft, GoChevronRight, GoPlus } from 'react-icons/go';
 import { AiOutlineCalendar } from 'react-icons/ai';
-import { useQuery } from 'react-query';
+import { useForm } from 'react-hook-form';
 import { useRecoilValue } from 'recoil';
-import styled from 'styled-components';
 import * as Styled from './styled';
 import * as PageStyled from '../pageStyled';
-import { fetchCalendarData } from '../../api';
 import { modalVisibleState } from '../../atoms';
+import CalendarComponent from '../../components/calendar';
 
 const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-const CalendarHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    font-size: 30px;
-    font-weight: 600;
-`;
-
-const CalendarBody = styled.div`
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    margin-top: 40px;
-    font-weight: 400;
-`;
-
-const Day = styled.span`
-    text-align: center;
-    margin-bottom: 5px;
-`;
-
-const DayBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100px;
-    padding: 12px;
-    border: 1px dashed #e8e8e8;
-    border-collapse: separate;
-    color: ${(props) => (props.prev ? 'lightgray' : '#6e6e6e')};
-    font-size: 13px;
-    .day {
-        align-self: flex-start;
-        margin-bottom: 6px;
-    }
-    .toDo {
-        color: ${(props) => (props.prev ? 'lightgray' : 'black')};
-        font-weight: 500;
-        margin-bottom: 2px;
-        @media screen and (max-width: 1023px) {
-            font-size: 11px;
-        }
-        @media screen and (max-width: 767px) {
-            font-size: 10px;
-            -webkit-transform: scale(0.8);
-        }
-    }
-`;
-
-const YearBox = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    @media screen and (max-width: 767px) {
-        font-size: 20px;
-    }
-`;
-
-const IconBox = styled.div`
-    padding-top: 2px;
-    margin: 0px 3px;
-    cursor: pointer;
-`;
-
-const HeaderLeft = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    svg {
-        font-size: 40px;
-    }
-    span {
-        margin-left: 10px;
-        padding-top: 5px;
-    }
-    @media screen and (max-width: 767px) {
-        font-size: 18px;
-        svg {
-            font-size: 35px;
-        }
-        span {
-            margin-left: 4px;
-        }
-    }
-`;
-
-const AddBtn = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-    border: 1px solid lightgray;
-    height: 30px;
-    width: 90px;
-    border-radius: 10px;
-    cursor: pointer;
-    svg {
-        margin-right: 10px;
-        height: 100%;
-    }
-`;
-
-function CalendarComponent({ date }) {
-    const { isLoading, data } = useQuery('calendarDatas', fetchCalendarData);
-    const today = date;
-    const startWeek = today.clone().startOf('month').week();
-
-    // eslint-disable-next-line operator-linebreak
-    const endWeek =
-        today.clone().endOf('month').week() === 1 ? 53 : today.clone().endOf('month').week();
-
-    const calendar = [];
-    const onClick = () => {
-        // delete post
-    };
-
-    for (let week = startWeek; week <= endWeek; week += 1) {
-        calendar.push(
-            Array(7)
-                .fill(0)
-                .map((n, i) => {
-                    const current = today.clone().week(week).startOf('week').add(i, 'day');
-                    const prev = current.format('D') > 10 && week === startWeek;
-                    const next = current.format('D') < 10 && week === endWeek;
-                    return (
-                        <DayBox key={current.format('D')} prev={prev || next}>
-                            <div className='day'>{current.format('D')}</div>
-                            <div className='toDo'>2022 2학기 종강</div>
-                            <div className='toDo'>막동</div>
-                            <div className='toDo'>일정</div>
-                        </DayBox>
-                    );
-                }),
-        );
-    }
-    return calendar;
-}
 function Calendar() {
     const modalVisible = useRecoilValue(modalVisibleState);
     const [date, setdate] = useState(() => moment());
+    const [toggleAddBox, setToggleAddBox] = useState(false);
+    const { register, handleSubmit } = useForm();
     // eslint-disable-next-line no-confusing-arrow
     const jumpToMonth = (num) =>
         // eslint-disable-next-line implicit-arrow-linebreak
         num ? setdate(date.clone().add(30, 'day')) : setdate(date.clone().subtract(30, 'day'));
+    const onValid = (data) => {
+        // console.log("post 하는부분");
+    };
+
     return (
         <PageStyled.Container modalVisible={modalVisible}>
             <div className='inner'>
                 <Styled.Container>
-                    <CalendarHeader>
-                        <HeaderLeft>
+                    <Styled.CalendarHeader>
+                        <Styled.HeaderLeft>
                             <AiOutlineCalendar />
                             <span>캘린더</span>
-                        </HeaderLeft>
+                        </Styled.HeaderLeft>
 
-                        <YearBox>
-                            <IconBox>
+                        <Styled.YearBox>
+                            <Styled.IconBox>
                                 <GoChevronLeft type='button' onClick={() => jumpToMonth(0)} />
-                            </IconBox>
+                            </Styled.IconBox>
 
                             <span>{`${date.format('Y')}년 ${date.format('M')}월`}</span>
-                            <IconBox>
+                            <Styled.IconBox>
                                 <GoChevronRight onClick={() => jumpToMonth(1)} />
-                            </IconBox>
-                        </YearBox>
-                        <AddBtn type='button'>
+                            </Styled.IconBox>
+                        </Styled.YearBox>
+                        <Styled.AddBtn
+                            type='button'
+                            onClick={() => setToggleAddBox((prev) => !prev)}
+                        >
                             <GoPlus />
                             일정추가
-                        </AddBtn>
-                    </CalendarHeader>
-                    <CalendarBody>
+                        </Styled.AddBtn>
+                    </Styled.CalendarHeader>
+                    <Styled.CalendarBody>
+                        {toggleAddBox && (
+                            <Styled.AddBox>
+                                <form onSubmit={handleSubmit(onValid)}>
+                                    <span>제목</span>
+                                    <input {...register('title')} />
+                                    <div>
+                                        날짜
+                                        <input
+                                            type='date'
+                                            min='2018-01-01'
+                                            {...register('startDate')}
+                                        />
+                                    </div>
+
+                                    <button type='submit'>저장</button>
+                                </form>
+                            </Styled.AddBox>
+                        )}
                         {days.map((day) => (
-                            <Day key={day}>{day}</Day>
+                            <Styled.Day key={day}>{day}</Styled.Day>
                         ))}
                         <CalendarComponent date={date} />
-                    </CalendarBody>
+                    </Styled.CalendarBody>
                 </Styled.Container>
             </div>
         </PageStyled.Container>
--- src/pages/calendar/styled.js
+++ src/pages/calendar/styled.js
@@ -5,3 +5,104 @@ export const Container = styled.div`
     height: 800px;
     margin-top: 100px;
 `;
+export const CalendarHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-size: 30px;
+    font-weight: 600;
+`;
+
+export const CalendarBody = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    margin-top: 40px;
+    font-weight: 400;
+`;
+
+export const Day = styled.span`
+    text-align: center;
+    margin-bottom: 5px;
+`;
+
+export const YearBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 767px) {
+        font-size: 20px;
+    }
+`;
+
+export const IconBox = styled.div`
+    padding-top: 2px;
+    margin: 0px 3px;
+    cursor: pointer;
+`;
+
+export const HeaderLeft = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+        font-size: 40px;
+    }
+    span {
+        margin-left: 10px;
+        padding-top: 5px;
+    }
+    @media screen and (max-width: 767px) {
+        font-size: 18px;
+        svg {
+            font-size: 35px;
+        }
+        span {
+            margin-left: 4px;
+        }
+    }
+`;
+
+export const AddBtn = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    border: 1px solid lightgray;
+    height: 30px;
+    width: 90px;
+    border-radius: 10px;
+    cursor: pointer;
+    svg {
+        margin-right: 6px;
+        height: 100%;
+    }
+`;
+
+export const AddBox = styled.div`
+    position: absolute;
+    z-index: 1;
+    background-color: white;
+    width: 250px;
+    height: 140px;
+    margin-top: -40px;
+    margin-left: 650px;
+    border-radius: 16px;
+    border: 1px solid lightgray;
+    padding: 20px;
+    opacity: 0.95;
+    input {
+        margin-left: 5px;
+    }
+    div {
+        margin-top: 15px;
+        margin-bottom: 15px;
+    }
+    button {
+        background-color: white;
+        border: 1px solid black;
+        font-size: 14px;
+        font-weight: 400;
+        border-radius: 4px;
+        width: 45px;
+        cursor: pointer;
+    }
+`;
