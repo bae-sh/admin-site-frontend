6bc12af0cbf3e7a2aa5d7211ecb4e2c6d71df6ec
bae-sh <hyeon1293@gmail.com>
2022-04-07 21:32:27 +0900

    .

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -4,11 +4,13 @@ import 'moment/locale/ko';
 import { GoChevronLeft, GoChevronRight, GoPlus } from 'react-icons/go';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import * as Styled from './styled';
 import * as PageStyled from '../pageStyled';
 import { modalVisibleState } from '../../atoms';
 import CalendarComponent from '../../components/calendar';
+import { url } from '../../api';
 
 const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
@@ -22,7 +24,23 @@ function Calendar() {
         // eslint-disable-next-line implicit-arrow-linebreak
         num ? setdate(date.clone().add(30, 'day')) : setdate(date.clone().subtract(30, 'day'));
     const onValid = (data) => {
-        // console.log("post 하는부분");
+        const startDate = data.startDate.split('-');
+        const body = {
+            title: data.title,
+            startDate: { year: startDate[0], month: startDate[1], dayOfMonth: startDate[2] },
+        };
+        console.log(body);
+        // axios({
+        //     method: 'post',
+        //     url: `${url}/calendar`,
+        //     data,
+        // })
+        //     .then((response) => {
+        //         console.log(response.data.data);
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
     };
 
     return (
