817e5e97f1f233d5688c2c79ced0bb2c18b22af1
bae-sh <hyeon1293@gmail.com>
2022-08-22 19:54:12 +0900

    chore : 상수 폴더로 이동

--- src/components/loginbox/LoginContainer.js
+++ src/components/loginbox/LoginContainer.js
@@ -7,21 +7,7 @@ import { useSetRecoilState } from 'recoil';
 import { userIdState } from '../../atoms';
 import * as Styled from './styled';
 import { fetchLogin } from '../../api';
-
-const inputList = [
-    {
-        name: 'Email',
-        id: 'email',
-        placeholder: 'Email를 입력하세요',
-        errorMsg: 'Email를 입력하세요',
-    },
-    {
-        name: 'Password',
-        id: 'password',
-        placeholder: 'Password를 입력하세요',
-        errorMsg: 'Password를 입력하세요',
-    },
-];
+import { inputList } from '../../constant/login';
 
 function LoginContainer() {
     const {
---
+++ src/constant/login.js
@@ -0,0 +1,16 @@
+const inputList = [
+    {
+        name: 'Email',
+        id: 'email',
+        placeholder: 'Email를 입력하세요',
+        errorMsg: 'Email를 입력하세요',
+    },
+    {
+        name: 'Password',
+        id: 'password',
+        placeholder: 'Password를 입력하세요',
+        errorMsg: 'Password를 입력하세요',
+    },
+];
+
+export { inputList };
